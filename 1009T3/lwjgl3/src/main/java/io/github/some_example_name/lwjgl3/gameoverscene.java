a0f7db6432fb186950f2a71f30d0aa203de194c4
ultraprint <dinobananope@protonmail.com>
2025-02-18 04:25:37 +0800

    re-added child scene classes

---
+++ 1009T3/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/gameoverscene.java
@@ -0,0 +1,39 @@
+package io.github.some_example_name.lwjgl3;
+
+import abstractengine.scene;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+
+public class gameoverscene extends scene{
+    public gameoverscene(){
+        super("Default Game Over Scene");
+    }
+
+    public gameoverscene(String name, Texture bgImg, Color bgColor, OrthographicCamera camera){
+		super(name,bgColor,bgImg,camera);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+        // Handle scene updates
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(super.getBgImg(), 0, 0);
+    }
+
+    @Override
+    public void dispose() {
+        super.getBgImg().dispose();
+    }
+}
