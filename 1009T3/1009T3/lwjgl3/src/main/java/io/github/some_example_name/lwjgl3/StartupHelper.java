3bcae05380793d5c4ea2da4a5f1d982225fcf716
Eugene Kuah <2400637@sit.singaporetech.edu.sg>
2025-02-18 14:23:15 +0800

    Delete 1009T3/1009T3 directory

--- 1009T3/1009T3/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/StartupHelper.java
+++
@@ -1,7 +0,0 @@
-package io.github.some_example_name.lwjgl3;
-
-public class StartupHelper {
-    public static void startNewJvmIfRequired() {
-        System.out.println("Starting new JVM instance if required...");
-    }
-}
